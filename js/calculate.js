0359283a13ab6c2cfde821d1aa1f07e783ac41e2
Dave Li <daveli@v1020-wn-222-131.campus-dynamic.uwaterloo.ca>
2016-03-26 11:54:47 -0400

    Added more formatting

--- js/calculate.js
+++ js/calculate.js
@@ -2,22 +2,23 @@
 
 $(function() {
 
-    $("input#expr").keypress(function (e) {
-        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-            $('button #exprsubmit').click();
-            return false;
-        } else {
-            return true;
-        }
-    });
+	$("input#expr").keypress(function(event) {
+	    if (event.which == 13) {
+	        event.preventDefault();
+	        $("button#exprsubmit").click();
+	    }
+	});
 
     $("button#exprsubmit").click(function() {
     	var expr = $("input#expr").val();
         var value = math.eval(expr);
         var n1 = math.floor(value/3);
         var n2 = value%value;
-       // var newv = parseInt(value, 10).toString(2)
-    	alert("clicked submit: " + n1 + " * 3 + " + n2);
+        
+        var retval = "" + n1 + " * 3 + " + n2;
+
+        $("#soln").show();
+        $("#soln").text(retval);
     });
 });
 

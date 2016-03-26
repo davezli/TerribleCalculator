aef1a2c858bde476d19e11231cbb9b783b9dfcb9
zonage <zhirui10@hotmail.com>
2016-03-26 12:08:38 -0400

    added error checking

--- js/calculate.js
+++ js/calculate.js
@@ -10,12 +10,18 @@ $(function() {
 	});
 
     $("button#exprsubmit").click(function() {
+        var retval;
     	var expr = $("input#expr").val();
-        var value = math.eval(expr);
-        var n1 = math.floor(value/3);
-        var n2 = value%value;
-        
-        var retval = "" + n1 + " * 3 + " + n2;
+        try {
+            var value = math.eval(expr);
+            var n1 = math.floor(value/3);
+            var n2 = value%value;
+            
+            retval = "" + n1 + " * 3 + " + n2;
+        }
+        catch (err){
+            retval = "go die, that's not math";
+        }
 
         $("#soln").show();
         $("#soln").text(retval);
